1387963371cb16e69219e9c220497369ff99ab52
Brian Travis <bztravis88@gmail.com>
2024-02-02 15:13:25 -0500

    Add basic Countdown

--- app/ramping-up/page.tsx
+++ app/ramping-up/page.tsx
@@ -1,25 +1,12 @@
-'use client';
-
-import { useState, useEffect } from 'react';
 import Clock from '@/components/Clock';
 import Countdown from '@/components/Countdown';
 import styles from './page.module.scss';
 
-const END_DATES = [new Date('May 28')] as const;
-
 const page = () => {
-  const [endDate, setEndDate] = useState<(typeof END_DATES)[number]>(
-    END_DATES[0]
-  );
   return (
     <div className={styles.container}>
       <Clock />
-      <Countdown endDate={endDate} />
-      <select>
-        {END_DATES.map((date) => (
-          <option key={date.toISOString()}>Until {date}</option>
-        ))}
-      </select>
+      <Countdown />
     </div>
   );
 };
--- components/Countdown/index.tsx
+++ components/Countdown/index.tsx
@@ -1,13 +1,53 @@
-type Props = {
-  endDate: Date;
-};
+'use client';
 
-const Countdown = ({ endDate }: Props) => {
-  return (
-    <>
-      <h3>{endDate.toISOString()}</h3>
-    </>
+import { useState, useEffect } from 'react';
+import { format, parse } from 'date-fns';
+import styles from './Countdown.module.scss';
+
+const END_DATES = [new Date('May 13'), new Date('May 28')] as const;
+const DATE_FORMAT_STR = 'MMM do';
+
+const Countdown = () => {
+  const [endDate, setEndDate] = useState<(typeof END_DATES)[number]>(
+    END_DATES[0]
   );
+  const [duration, setDuration] = useState(0);
+
+  const updateDuration = () => {
+    setDuration((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    updateDuration();
+    const interval = setInterval(() => {
+      updateDuration();
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h3>{duration}</h3>
+      <p>Until </p>
+      <select
+        value={dateToReadable(endDate)}
+        onChange={(e) => {
+          setEndDate(parse(e.target.value, DATE_FORMAT_STR, new Date()));
+        }}
+      >
+        {END_DATES.map((date) => (
+          <option key={date.toISOString()}>{dateToReadable(date)}</option>
+        ))}
+      </select>
+    </div>
+  );
+
+  function dateToReadable(date: Date) {
+    return format(date, DATE_FORMAT_STR);
+  }
 };
 
 export default Countdown;
