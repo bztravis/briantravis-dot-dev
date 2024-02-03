c33de247ce640166f8d92ff09be2a05e02d1275a
Brian Travis <bztravis88@gmail.com>
2024-02-02 21:47:04 -0500

    Update text phase in animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import localFont from 'next/font/local';
+import { styleBuilder } from '@/utils/styleBuilder';
 import styles from './page.module.scss';
+
+const CabinetGrotesk = localFont({
+  src: '../public/fonts/CabinetGrotesk-Variable.ttf',
+  display: 'swap',
+});
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styleBuilder([styles.container, CabinetGrotesk.className])}>
       <p>brian travis [dot] dev</p>
-      <h1>Coming</h1>
-      <h1>Soon</h1>
+      <div>
+        <h1>Coming</h1>
+        <h1>Soon</h1>
+      </div>
     </div>
   );
 }
--- app/ramping-up/page.tsx
+++ app/ramping-up/page.tsx
@@ -1,17 +1,10 @@
-import localFont from 'next/font/local';
-import { styleBuilder } from '@/utils/styleBuilder';
 import Clock from '@/components/Clock';
 import Countdown from '@/components/Countdown';
 import styles from './page.module.scss';
 
-const CabinetGrotesk = localFont({
-  src: '../../public/fonts/CabinetGrotesk-Variable.ttf',
-  display: 'swap',
-});
-
 const page = () => {
   return (
-    <div className={styleBuilder([styles.container, CabinetGrotesk.className])}>
+    <div className={styles.container}>
       <Clock />
       <Countdown />
     </div>
