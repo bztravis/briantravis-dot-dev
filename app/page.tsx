b7d3559445efe4f0e93cbf86dce83c1ec775a3e2
Brian Travis <bztravis88@gmail.com>
2024-02-02 22:21:17 -0500

    Fix font populating to all pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-import localFont from 'next/font/local';
-import { styleBuilder } from '@/utils/styleBuilder';
 import styles from './page.module.scss';
-
-const CabinetGrotesk = localFont({
-  src: '../public/fonts/CabinetGrotesk-Variable.ttf',
-  display: 'swap',
-});
 
 export default function Home() {
   return (
-    <div className={styleBuilder([styles.container, CabinetGrotesk.className])}>
+    <div className={styles.container}>
       <div>
         <p>brian travis [dot] dev</p>
         <div className={styles.links}>
