2af9fd3fe01eccee69f63a1d8029901e47e3dc1a
Brian Travis <bztravis88@gmail.com>
2024-02-02 19:11:34 -0500

    Add misc styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import styles from './page.module.scss';
 
 export default function Home() {
-  return <div className={styles.container}>brian travis [dot] dev</div>;
+  return (
+    <div className={styles.container}>
+      <p>brian travis [dot] dev</p>
+      <h1>Coming</h1>
+      <h1>Soon</h1>
+    </div>
+  );
 }
