3fb4f275f92698c2187ad88690c33fe71912f4fb
Brian Travis <bztravis@umich.edu>
2024-02-03 09:28:23 -0500

    Add metadata titles to pages

--- app/ramping-up/page.tsx
+++ app/ramping-up/page.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from 'next';
 import Clock from '@/components/Clock';
 import Countdown from '@/components/Countdown';
 import styles from './page.module.scss';
+
+export const metadata: Metadata = {
+  title: 'Ramping Up',
+  description: '',
+};
 
 const page = () => {
   return (
