7f0872b251bf06457eed2fa7c3f628436fe7a804
Brian Travis <bztravis88@gmail.com>
2024-02-03 21:14:17 -0500

    Add link preview

--- app/ramping-up/page.tsx
+++ app/ramping-up/page.tsx
@@ -6,6 +6,21 @@ import styles from './page.module.scss';
 export const metadata: Metadata = {
   title: 'Ramping Up',
   description: '',
+  openGraph: {
+    title: 'Ramping Up',
+    description: 'Countdown to intern start dates at Ramp',
+    url: 'https://briantravis.dev/ramping-up',
+    siteName: 'Brian Travis',
+    images: [
+      {
+        url: 'https://lh3.googleusercontent.com/drive-viewer/AEYmBYQMICJJiIwPmQ8USg-_GJiD9JfN76q46U-igf4nkTaVJABVMh6OVZNBQb57Y93Pn2oRTPAPhAsX3mvMQad-2t_Rv4Zouw=s2560', // Must be an absolute URL
+        width: 578,
+        height: 400,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 const page = () => {
