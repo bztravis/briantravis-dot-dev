5110ac627df0d3b7f7265dd15a467c89f7aa5501
Cooper Saye <coopersaye@gmail.com>
2024-02-04 19:37:01 -0500

    Fix default end date

--- components/Countdown/index.tsx
+++ components/Countdown/index.tsx
@@ -14,7 +14,7 @@ const END_DATES = [
   new Date('May 13, 2024'),
   new Date('May 28, 2024'), // default
 ] as const;
-const DEFAULT_END_DATE = END_DATES[2];
+const DEFAULT_END_DATE = END_DATES[3];
 
 const DATE_FORMAT_STR = 'MMM do';
 
